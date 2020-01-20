eef4ce6c792e03493f102650d5ced475715b947b
shiva.nr-at-615338394175 <shiva.nr@thresholdsoft.com>
2020-01-20 15:56:07 +0530

    Demo Commit 1

---
+++ SampleMailApplication/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EASendMail;
+
+namespace SampleMailApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                SmtpMail oMail = new SmtpMail("TryIt");
+
+                // Your gmail email address
+                oMail.From = "venkat452143@gmail.com";
+                // Set recipient email address
+                oMail.To = "venkata.r@thresholdsoft.com";
+
+                // Set email subject
+                oMail.Subject = "test email from gmail account";
+                // Set email body
+                oMail.TextBody = "this is a test email sent from c# project with gmail.";
+
+                // Gmail SMTP server address
+                SmtpServer oServer = new SmtpServer("smtp.gmail.com");
+
+                // Gmail user authentication
+                // For example: your email is "gmailid@gmail.com", then the user should be the same
+                oServer.User = "yourmailid@gmail.com";
+                oServer.Password = "yourpassword";
+
+                // Set 465 port
+                oServer.Port = 465;
+
+                // detect SSL/TLS automatically
+                oServer.ConnectType = SmtpConnectType.ConnectSSLAuto;
+
+                Console.WriteLine("start to send email over SSL ...");
+
+                SmtpClient oSmtp = new SmtpClient();
+                oSmtp.SendMail(oServer, oMail);
+
+                Console.WriteLine("email was sent successfully!");
+            }
+            catch (Exception ep)
+            {
+                Console.WriteLine("failed to send email with the following error:");
+                Console.WriteLine(ep.Message);
+            }
+        }
+    }
+}
